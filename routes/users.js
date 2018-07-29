c2d0613473eb92146fb6c3d8c414aac5b2080160
aarizirf <30647306+aarizirf@users.noreply.github.com>
2018-07-29 15:49:13 -0700

    Changed to firestore

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require("express"),
-  router = express.Router();
-
-router.get("/login", function(req, res) {
-  var page = "/login";
-  res.render("users/login", { page: "/login" });
-});
-
-module.exports = router;
